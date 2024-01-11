c24890bd7bf1ebb6b6853f634689fa42c89a6980
ledinhthachquang <20521803@gm.uit.edu.vn>
2024-01-12 00:12:20 +0700

    [Update] Navigation Bar

--- App.js
+++ App.js
@@ -3,34 +3,82 @@ import { StyleSheet, Text, View } from 'react-native';
 import { collection, getDocs } from "firebase/firestore"; 
 import { storage } from './firebaseConfig';
 import { useEffect } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+
+
+const homeName = "Home";
+const searchName = "Search";
+const settingsName = "Settings";
+const addName = "Add";
+const folderName = "Library";
+
+const Tab = createBottomTabNavigator();
+
+// Dummy Screen Component
+const DummyScreen = ({ screenName }) => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>{screenName}</Text>
+  </View>
+);
 
 export default function App() {
-  const fetchData = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(storage, "hihi"));
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-      });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-  useEffect(()=>{
-fetchData()
-  },[])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === searchName) {
+              iconName = focused ? "search" : "search-outline";
+            } else if (rn === settingsName) {
+              iconName = focused ? "settings" : "settings-outline";
+            } else if (rn === addName) {
+              iconName = focused ? "add-circle-sharp" : "add-circle-outline";
+              size = 45;
+            } else if (rn === folderName) {
+              iconName = focused ? "folder" : "folder-outline";
+            }
+            return (
+              <Ionicons
+                style={{ marginTop: 5 }}
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          },
+          tabBarActiveTintColor: "#fff",
+          tabBarInactiveTintColor: "#4B526C",
+          tabBarLabelStyle: { fontSize: 10, fontWeight: '700', marginBottom: 0 },
+          tabBarStyle: {
+            backgroundColor: "#0A092B",
+            borderTopColor: "#2C2D42",
+            borderTopWidth: 2,
+            borderStyle: "solid",
+            height: 80,
+          },
+          headerShown: false
+        })}
+      >
+        <Tab.Screen name={homeName} children={() => <DummyScreen screenName={homeName} />} />
+        <Tab.Screen name={searchName} children={() => <DummyScreen screenName={searchName} />} />
+        <Tab.Screen
+          name={addName}
+          children={() => <DummyScreen screenName={addName} />}
+          options={{ tabBarLabelStyle: { display: "none" } }}
+        />
+        <Tab.Screen name={folderName} children={() => <DummyScreen screenName={folderName} />} />
+        <Tab.Screen name={settingsName} children={() => <DummyScreen screenName={settingsName} />} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
