c19b388d3d9306a583c966ef98b685de360dcf91
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2024-01-11 22:52:24 +0700

    set up firebase

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { collection, getDocs } from "firebase/firestore"; 
+import { storage } from './firebaseConfig';
+import { useEffect } from 'react';
 
 export default function App() {
+  const fetchData = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(storage, "hihi"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  useEffect(()=>{
+fetchData()
+  },[])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ firebaseConfig.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+import { getFirestore } from "firebase/firestore";
+
+
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDOMEcVnwkTCrMWNGt1XqB8E71gTDTtOy0",
+    authDomain: "studyapp-5903b.firebaseapp.com",
+    projectId: "studyapp-5903b",
+    storageBucket: "studyapp-5903b.appspot.com",
+    messagingSenderId: "920224478087",
+    appId: "1:920224478087:web:9aea5fc60688097be6d922",
+    measurementId: "G-9EWS4H8J5W"
+  };
+  
+
+const app = initializeApp(firebaseConfig);
+const storage = getFirestore(app);
+
+export {storage}
