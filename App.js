e7aecabafc7af827d9fd3854b214ef853f562077
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2024-01-12 13:35:43 +0700

    [Fix] nav bar

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import NavBar from "./component/NavBar";
+import { Navigation } from "./component/Navigation";
 
 
 export default function App() {
   return (
     <Provider store={store}>
-     <NavBar/>
+     <Navigation/>
     </Provider>
   );
 }
