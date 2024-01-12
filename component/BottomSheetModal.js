148fa1517c1eb53eb0bfcbc944a2eb1a36231517
ledinhthachquang <20521803@gm.uit.edu.vn>
2024-01-12 15:43:00 +0700

    [Update] BottomSheetModal

--- component/BottomSheetModal.js
+++ component/BottomSheetModal.js
@@ -23,7 +23,7 @@ const BottomSheetModal = ({ isVisible, onClose, onTakePhoto, onSelectImage }) =>
     onSelectImage();
   };
 
-  // Function to render an item with an icon and text
+ 
   const renderItem = (iconName, text, onPress) => (
     <TouchableOpacity style={styles.item} onPress={onPress}>
       <Icon name={iconName} size={24} color="#ffffff" />
@@ -31,10 +31,9 @@ const BottomSheetModal = ({ isVisible, onClose, onTakePhoto, onSelectImage }) =>
     </TouchableOpacity>
   );
 
-  // Define the snap points based on the visibility
-  const snapPoints = useMemo(() => (isVisible ? ['25%', '50%'] : ['10%']), [isVisible]);
 
-  // Custom handle component
+  const snapPoints = useMemo(() => (isVisible ? ['30%', '50%'] : ['10%']), [isVisible]);
+
   const CustomHandle = () => (
     <View style={styles.handleContainer}>
       <TouchableOpacity style={styles.handle} onPress={closeBottomSheet} />
@@ -48,12 +47,12 @@ const BottomSheetModal = ({ isVisible, onClose, onTakePhoto, onSelectImage }) =>
       snapPoints={snapPoints}
       enablePanDownToClose={true}
       onClose={closeBottomSheet}
-      handleComponent={CustomHandle} // Use the custom handle component
-      // rest of your props
+      handleComponent={CustomHandle} 
+
     >
       <View style={styles.container}>
-        {renderItem("camera", "Máy ảnh", handleTakePhoto)}
-        {renderItem("images", "Chọn ảnh từ thư viện", handleSelectImage)}
+        {renderItem("folder", "Học phần", handleTakePhoto)}
+        {renderItem("file-tray-stacked", "Thư mục", handleSelectImage)}
       </View>
     </BottomSheet>
   );
@@ -61,28 +60,29 @@ const BottomSheetModal = ({ isVisible, onClose, onTakePhoto, onSelectImage }) =>
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#2E3856', // Your custom background color
+    backgroundColor: '#2E3856', 
     flex: 1,
     alignItems: 'center',
-    padding: 20, // Padding to ensure content is not too close to the edges
-    paddingTop: 30, // Increased padding at the top to create more space
-    paddingBottom: 30, // Reduced padding at the bottom
+    padding: 20, 
+    paddingTop: 30, 
+    paddingBottom: 30, 
     height:228,
   },
   item: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#4B5673', // The color of your item background
-    borderRadius: 10,
+    backgroundColor: '#4B5673', 
+    borderRadius: 13,
     paddingVertical: 15,
     paddingHorizontal: 20,
-    width: '100%', // Full width items
-    marginVertical: 10, // Margin between items
+    width: '100%', 
+    marginVertical: 10, 
   },
   text: {
     color: '#ffffff',
     marginLeft: 10,
     fontSize: 16,
+    fontWeight: 'bold',
   },
   handleContainer: {
     backgroundColor:'#2E3856',
@@ -90,14 +90,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderTopRightRadius:15,
     borderTopLeftRadius:15,
-    paddingTop: 10, // Space above the handle
+    paddingTop: 0,
   },
   handle: {
-    width: 40, // Width of the handle
-    height: 5, // Height of the handle
+    width: 40,
+    height: 5,
     borderRadius: 3,
     backgroundColor: '#747D9F',
-    marginBottom: 10, // Space below the handle
+    marginTop: 10, 
   },
 });
 
