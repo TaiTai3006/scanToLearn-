72f0146c4824e224c47379072ea00910c2a91e07
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2024-01-12 22:58:10 +0700

    [Fix] Folder

--- screen/FolderDetailScreen.js
+++ screen/FolderDetailScreen.js
@@ -1,74 +1,94 @@
+import React from "react";
 import { View, SafeAreaView, Text, StyleSheet, FlatList } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { Button } from "@rneui/themed";
 import TermCard from "../component/TermCard";
+
 const FolderDetailScreen = () => {
   return (
-    <View style={{ backgroundColor: "#0A092D", flex: 1 }}>
-      <SafeAreaView style={{ marginLeft: 20, marginRight: 20 }}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+    <View style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.header}>
           <View>
             <Ionicons color="#fff" size={30} name="arrow-back-outline" />
           </View>
-          <View style={{ flexDirection: "row" }}>
+          <View style={styles.headerIcons}>
             <Ionicons
               color="#fff"
               size={30}
               name="add-outline"
-              style={{ marginRight: 20 }}
+              style={styles.iconMargin}
             />
             <Ionicons color="#fff" size={30} name="ellipsis-horizontal-sharp" />
           </View>
         </View>
         <View>
-          <Text style={[styles.text, { marginTop: 50 }]}>huhu</Text>
-          <Text style={{ fontSize: 15, color: "#fff", marginTop: 15, marginBottom: 40 }}>
-            0 terms
-          </Text>
+          <Text style={[styles.text, styles.marginTop50]}>huhu</Text>
+          <Text style={styles.infoText}>0 terms</Text>
         </View>
-        
-        {[...Array(1)].length == 0 ?
-        <View style={styles.card}>
-          <Text style={[styles.text, { fontSize: 16, textAlign: "center" }]}>
-            This folder haven't the terms
-          </Text>
-          <Text
-            style={{
-              fontSize: 15,
-              color: "#fff",
-              marginTop: 15,
-              textAlign: "center",
-            }}
-          >
-            Add sets to this folder for organization stack them.
-          </Text>
-          <View style={{ flexDirection: "row", justifyContent: "center" }}>
-            <Button
-              style={{ marginTop: 20 }}
-              titleStyle={{ fontSize: 15 }}
-              buttonStyle={{ backgroundColor: "#A8B1FF" }}
-            >
-              Add term
-            </Button>
+
+        {[...Array(1)].length === 0 ? (
+          <View style={styles.card}>
+            <Text style={[styles.text, styles.centerText, styles.fontSize16]}>
+              This folder hasn't any terms
+            </Text>
+            <Text style={[styles.infoText, styles.centerText]}>
+              Add sets to this folder for organization stack them.
+            </Text>
+            <View style={styles.centerRow}>
+              <Button
+                style={styles.buttonMarginTop}
+                titleStyle={styles.buttonText}
+                buttonStyle={styles.button}
+              >
+                Add term
+              </Button>
+            </View>
           </View>
-        </View> :
-         <FlatList
-         data={[...Array(10)]}
-         renderItem={({ item }) => <TermCard  folderType={true}/>}
-         keyExtractor={item => item}
-         
-       />}
+        ) : (
+          <FlatList
+            data={[...Array(10)]}
+            renderItem={({ item }) => <TermCard folderType={true} />}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        )}
       </SafeAreaView>
     </View>
   );
 };
 
-export default FolderDetailScreen;
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#0A092D",
+    flex: 1,
+  },
+  safeArea: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  headerIcons: {
+    flexDirection: "row",
+  },
+  iconMargin: {
+    marginRight: 20,
+  },
   text: {
     color: "#fff",
     fontWeight: "bold",
     fontSize: 30,
+  },
+  marginTop50: {
+    marginTop: 50,
+  },
+  infoText: {
+    fontSize: 15,
+    color: "#fff",
+    marginTop: 15,
+    marginBottom: 40,
   },
   card: {
     paddingTop: 50,
@@ -77,4 +97,25 @@ const styles = StyleSheet.create({
     paddingRight: 50,
     backgroundColor: "#586380",
   },
+  centerText: {
+    textAlign: "center",
+  },
+  fontSize16: {
+    fontSize: 16,
+  },
+  centerRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonMarginTop: {
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 15,
+  },
+  button: {
+    backgroundColor: "#A8B1FF",
+  },
 });
+
+export default FolderDetailScreen;
